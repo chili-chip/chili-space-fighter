442b4d8e13dcba473ad8d2fa45745a77eb410180
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2026-03-01 16:34:53 +0100

    organize project

---
+++ src/game.cpp
@@ -0,0 +1,15 @@
+#include "game.hpp"
+
+using namespace blit;
+
+
+void init() {
+    set_screen_mode(ScreenMode::hires);
+}
+
+void render(uint32_t time) {
+    screen.clear();
+}
+
+void update(uint32_t time) {
+}
---
+++ src/game.hpp
@@ -0,0 +1,5 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+#include "32blit.hpp"
+
+#endif // GAME_HPP
